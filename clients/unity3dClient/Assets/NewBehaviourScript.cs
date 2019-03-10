29d6f7c2a3b09552b89c3e6cdf529f04abbd4fd0
Bukach Maksim <it.vimax@gmail.com>
2019-03-10 17:08:49 +0200

    first commit

---
+++ clients/unity3dClient/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using socket.io;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("My Start");
+		var serverUrl = "http://localhost:3000";
+		var socket = Socket.Connect(serverUrl);
+		
+		
+		// receive "news" event
+		socket.On("chatevent", (string data) => {
+			Debug.Log(data);
+
+			// Emit raw string data
+			socket.Emit("chatevent", "{ my: data }");
+
+			// Emit json-formatted string data
+			socket.EmitJson("chatevent2", @"{ ""my"": ""data"" }");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
